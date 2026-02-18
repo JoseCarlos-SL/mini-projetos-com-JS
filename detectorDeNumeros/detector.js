cf5bd13aec2d4ae9f9b72a9a21a1dccb02e19964
JoseCarlos-SL <josecarlos.dev22@gmail.com>
2026-02-18 10:07:05 -0300

    Detector de números

---
+++ detectorDeNumeros/detector.js
@@ -0,0 +1,14 @@
+let numero = 5
+
+if(numero > 0 && numero % 2 === 0)
+{
+    console.log("Positivo e par")
+}
+else if(numero > 0 && numero % 2 !== 0)
+{
+    console.log("Positivo e Ímpar")
+}
+else
+{
+    console.log("número negativo ou igual a zero")
+}
