9626c364b37de448a30ea0d8accdd0a8e41deb42
JoseCarlos-SL <josecarlos.dev22@gmail.com>
2026-02-18 09:57:44 -0300

    sistema de acesso com variáveis já definidas

---
+++ sistemaDeAcesso/acesso.js
@@ -0,0 +1,11 @@
+let usuario = "JoséCarlos"
+let senha = "@@12344321@@"
+
+if (usuario === "JoséCarlos" && senha === "@@12344321@@")
+{
+    console.log("Entrou")
+}
+else
+{
+    console.log("Não entrou")
+}
